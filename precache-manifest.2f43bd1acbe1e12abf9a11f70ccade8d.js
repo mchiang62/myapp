35d74f48d3bc82d0b2551635c1da0b20a00683b8
Melissa Chiang <mchiang62@gmail.com>
2020-01-13 21:17:11 -0500

    Updates

---
+++ precache-manifest.2f43bd1acbe1e12abf9a11f70ccade8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6769d98a05a8b2005df94b2605c04995",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "40db45efa33c44487615",
+    "url": "/my-app/static/css/main.e3bb4cb3.chunk.css"
+  },
+  {
+    "revision": "c586403b1f342a4a93ed",
+    "url": "/my-app/static/js/2.16dedccb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.16dedccb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40db45efa33c44487615",
+    "url": "/my-app/static/js/main.2cd1464d.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
