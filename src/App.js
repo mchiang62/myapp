6ca4712a7513329d0e9b1ba95966c280a8bbe3d9
Melissa Chiang <mchiang62@gmail.com>
2020-02-04 23:46:24 -0500

    cards showing in rows and removed some component folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import logo from "./logo.svg";
 import Header from "./Components/Header/Header";
-import Instruction from "./Components/Instruction/Instruction";
-// import Cards from "./Components/Cards/Cards";
 import Wrapper from "./Components/Wrapper/Wrapper";
 import Card from "./Components/Card/Card";
 import cardDisplay from "./cardDisplay.json";
@@ -17,58 +14,23 @@ class App extends Component {
     return (
       <Wrapper>
         <div className="container">
-          <h1> Memory Game </h1> <h4> Do not click the same twice! </h4>
-          {this.state.cardDisplay.map(cards => {
-            return (
-              <div className="row">
-                <Card id={cards.id} image={cards.URL} />
-              </div>
-            );
-          })}
+          <div className="row">
+            <Header>
+            <h1> Memory Game </h1> 
+            <br />
+            <h4>Do not click the same twice! </h4>
+            </Header>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            {this.state.cardDisplay.map(cards => {
+              return <Card id={cards.id} image={cards.URL} />;
+            })}
+          </div>
         </div>
       </Wrapper>
     );
   }
 }
 export default App;
-
-// alt={props.name}
-//src={props.image}
-//           id={props.id}
-
-// export default function App() {
-//   return (
-//     <div className="App">
-//       <Header>
-//         <h1> Memory Game </h1> <h4> Do not click the same twice! </h4>
-//       </Header>
-//       <Instruction> Scores: </Instruction>
-//       <Cards> </Cards>
-//       <Wrapper>
-//         {cardDisplay.map(picture => (
-//           <Card key={picture.id} image={picture.URL} />
-//         ))}
-//       </Wrapper>
-//     </div>
-//   );
-// }
-
-// render() {
-//   return (
-//     <Wrapper>
-//       <Title>Friends List</Title>
-//       {this.state.friends.map(friend => (
-//         <FriendCard
-//           removeFriend={this.removeFriend}
-//           id={friend.id}
-//           key={friend.id}
-//           name={friend.name}
-//           image={friend.image}
-//           occupation={friend.occupation}
-//           location={friend.location}
-//         />
-//       ))}
-//     </Wrapper>
-//   );
-// }
-// }
--- src/Components/Instruction/Instruction.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Instruction.css";
-
-function Instruction (props) {
-    return <h1 className="instruction">{props.children}</h1>;
-  }
-  
-  export default Instruction;
